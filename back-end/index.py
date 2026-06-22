f0d347b9a3a449a7313d4848b66871ac9302920b
OttoMoraes <otto.moraes0404@gmail.com>
2026-06-22 14:31:15 +0000

    instalando requests e TACO

--- back-end/index.py
+++ back-end/index.py
@@ -7,3 +7,20 @@ dados = requests.get(url).json()
 
 for estado in dados:
     print(estado["nome"])
+
+import requests
+
+codigo = "7891000100103"
+
+url = f"https://world.openfoodfacts.org/api/v0/product/{codigo}.json"
+
+produto = requests.get(url).json()
+
+print(produto["product"]["product_name"])
+
+import pandas as pd
+
+df = pd.read_excel("TACO.xlsx")
+
+print(df.head())
+print(df.columns)
